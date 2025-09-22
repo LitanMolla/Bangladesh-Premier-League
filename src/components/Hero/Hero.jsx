adc13d7f53e668f8acc3a5367a6e4c02a0d37e2a
LitanMolla <litanmolla9@gmail.com>
2025-09-22 18:37:15 +0600

    bg image usse solve

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Container from '../Container/Container'
 import banner from '../../assets/banner-main.png'
+import bg from '../../assets/bg-shadow.png'
 
 const Hero = () => {
   return (
     <>
       <Container>
-        <div className={`bg-[url('../../assets/bg-shadow.png')] py-30 bg-black bg-cover text-center space-y-3 rounded-xl overflow-hidden mb-5`}>
+        <div style={{backgroundImage:`url(${bg})`}} className={` py-30 bg-black bg-cover text-center space-y-3 rounded-xl overflow-hidden mb-5`}>
           <img className='mx-auto' src={banner} alt="Banner" />
           <h2 className='md:text-4xl text-3xl font-bold text-gray-100'>Assemble Your Ultimate Dream 11 Cricket Team</h2>
           <h4 className='md:text-2xl text-xl font-bold text-gray-400'>Beyond Boundaries Beyond Limits</h4>
